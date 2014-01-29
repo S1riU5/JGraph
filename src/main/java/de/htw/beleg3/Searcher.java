7030d994dd437baac519dfaf171c5b717cfb8cff
sebsch <s0543196@htw-berlin.de>
2014-01-29 15:00:56 +0100

    merging

--- src/main/java/de/htw/beleg3/Searcher.java
+++ src/main/java/de/htw/beleg3/Searcher.java
@@ -8,27 +8,7 @@ public class Searcher {
 		 */
 	}
 	
-	public int tiefensuche(int[][] adjMat, boolean [] alreadyVisited, int current , int target) {
-		// [start , 1, 3, 56, end]
-		
-		/*
-		 */
-		alreadyVisited[current] = true;
-
-		if (current == target ){		
-			return (current); 
-		}
-		
-		for (int i = 0; i < adjMat[0].length; i++){
-			if (! alreadyVisited[current+i] && adjMat[current][i] >= 0){
-				tiefensuche(adjMat,  alreadyVisited, current+1, target);
-
-			}
-		}
-		System.err.println("NO CONNECTION FOUND!");
-		//FIXME
-		return -1;
-	}
+	
 	
 	private int[] backtrack(boolean[] ways){
 		for (int i = 0; i< ways.length; i++){
