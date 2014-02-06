72675b79db1f8507f138e7b3dbb84ac34868bd44
sebsch <s0543196@htw-berlin.de>
2014-02-06 18:12:26 +0100

    commenting

--- src/main/java/de/htw/beleg3/Searcher.java
+++ src/main/java/de/htw/beleg3/Searcher.java
@@ -5,19 +5,28 @@ import java.util.ArrayList;
 
 public class Searcher {
 
-	
-	
-	public Searcher(){
-	
-	}
+	/**
+	 * class Searcher
+	 */
 	
 
-	
 	public int[][] dms (int[][] adjMat, int current, int target){
 		/**
+		 * dms()
+		 *
+		 * depth-first search
+		 *
+		 * @return int[][]    pathtree to target.
 		 * 
+		 * @param int[][] adjMat   adjacencyMatrix
+		 * @param int current   startnode
+		 * @param int targhet   targetnode
 		 */
-		
+		 /*
+		  *  Calling dms with backtracking:
+		  * 
+		  *  backtrack(target, dms(adjMat, start, target))
+		  */		
 		Stack st = new Stack(); 
 		Stack fst = new Stack(); // fromstack
 		boolean[] discovered = new boolean[adjMat.length];
@@ -28,20 +37,15 @@ public class Searcher {
 		int itert = 0;
 		st.push(current);
 		fst.push(current);
+		
 		while ( !st.isEmpty() && current != target ){
-			// Ok the problem is:
-			//
-			// If a edge is gone the second time
-			// the value is saved and will be writen
-			// in way[0][n]
 			way[0][itert] = fst.pop();
-			current = st.pop();
+			current = st.pop();	
 			if (!discovered[current]){
 				discovered[current] = true;				
 				way[1][itert] = current;
-				System.out.printf("\t%s\t-\t%s\t(%s)\n", 
-						way[0][itert], way[1][itert], itert);
 				itert++;
+				
 				for (int i = 0; i < adjMat.length; i++){
 					if ( !(adjMat[i][current] < 0) && ! discovered[i]){
 						st.push(i);
@@ -54,6 +58,15 @@ public class Searcher {
 	}
 	
 	public Integer[] backtrack( int target, int[][] way){
+		/**
+		 * backtrack()
+		 * 
+		 * backtrackmethode to find ways in searchtrees.
+		 * 
+		 * @return Integer[]  Sorted array of nodes
+		 * @param int target   targetnode
+		 * int [][] way   searchtree
+		 */
 		System.out.println();
 		Integer[] bt = new Integer[way[0].length];
 		int cnt = 0;
@@ -75,24 +88,52 @@ public class Searcher {
 	
 	
 	private class Stack {	
-		ArrayList<Integer> stack;
+		/**
+		 * class Stack
+		 */
+		private ArrayList<Integer> stack;
 		
 		public  Stack(){
+			 /**
+			  * Stack()
+			  *
+			  * Simple implementation of a Stack.
+			  */
 			 stack = new ArrayList<Integer>();
 		}
 		
 		public void push(int elem){
+			/**
+			 * push()
+			 * 
+			 * Pushing object on stack.
+			 * 
+			 * @param int elem    
+			 */
 			stack.add(elem);
 		}
 		
 		public int pop(){
+			/**
+			 * pop()
+			 * 
+			 * Popping element from stack.
+			 * 
+			 * @return int    elem
+			 */
 			int elem = (Integer) stack.get(stack.size()-1);
 			stack.remove(stack.size()-1);
 			return elem;
 		}
 		
 		 public boolean isEmpty(){
-			 //System.out.println(stack.size());
+			 /**
+			  * isEmpty()
+			  * 
+			  * is Stack empty ?
+			  *
+			  * @return boolean   emptystack
+			  */
 			 return stack.size() <= 0;
 		 }
 		
